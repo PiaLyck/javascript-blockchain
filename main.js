603cf0c8e365678cf10ac8cd6c2f4da69e492543
Pia Lyck Festersen <pialyck@gmail.com>
2018-01-23 15:54:28 +1100

    add method for mining block, add description of 'nonce', add finishing touches

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = this.calculateHash(); // contains the hash of the block. 
+        this.hash = this.calculateHash(); // contains the hash of the block.
+        this.nonce = '0'; // See Proof-of-work comment
     }
 
     /*  Method for calculating the hash of the block, based on the block's properties.
@@ -23,8 +24,32 @@ class Block {
     */
     calculateHash() {
         // Returns a SHA256 hash (casted to a string)
-        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data) + this.nonce).toString();
     }
+
+
+    /* PROOF-OF-WORK MECHANISM
+        To add security and avoid spamming the blockchain, the user has to prove that she/he put a lot of computing power into making a block.
+        This process is also called 'mining'.
+        Example: BitCoin requires the hash of a block to begin with a number of zeros, 
+        so you have to run the hash function multiple times until you get lucky.
+        This is also called 'the difficulty'. This way, there is a steady amount of blocks being created.
+
+        Nonce: Nonces are used in proof-of-work systems to vary the input to a cryptographic hash function,
+        so as to obtain a hash for a certain input that fulfills certain arbitrary conditions.
+        Any change to the block data (such as the nonce) will make the block hash completely different.
+        For more, see https://en.wikipedia.org/wiki/Cryptographic_nonce#Hashing
+    */
+
+    mineBlock(difficulty) {
+        // Continue running until the block's hash starts with x amount of zeros (x = difficulty).
+        while (this.hash.substring('0', difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log('Block mined: ' + this.hash + " Nonce: " + this.nonce);
+    }
+
 }
 
 // Create class Blockchain
@@ -34,6 +59,7 @@ class Blockchain {
     */
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
 
     //  The first block on a blockchain is called a 'Genesis Block' and should be added manually
@@ -53,7 +79,10 @@ class Blockchain {
         newBlock.previousHash = this.getLatestBlock().hash;
 
         // Step 2. Recalculate the hash. Must be done every time a block's properties changes, eg. when setting the previousHash in Step 1.
-        newBlock.hash = newBlock.calculateHash();
+        // newBlock.hash = newBlock.calculateHash();
+
+        // Step 2 - Mine the block, using the constructors difficulty.
+        newBlock.mineBlock(this.difficulty);
 
         // Step 3. Push block onto chain
         this.chain.push(newBlock);
@@ -83,14 +112,6 @@ class Blockchain {
     }
 }
 
-/* PROOF-OF-WORK MECHANISM
-    To add security and avoid spamming the blockchain, the user has to prove that she/he put a lot of computing power into making a block.
-    This process is also called 'mining'.
-    Example: BitCoin requires the hash of a block to begin with a number of zeros, 
-    so you have to run the hash function multiple times until you get lucky.
-    This is also called 'the difficulty'. This way, there is a steady amount of blocks being created.
-*/
-
 
 /* TESTING TESTING */
 
@@ -98,13 +119,19 @@ class Blockchain {
 let sustainiaCoin = new Blockchain();
 
 // Add 2 blocks to the blockchain
+console.log('Mining block 1....');
 sustainiaCoin.addBlock(new Block("1", "21/01/2018", {
     amount: 6
 }));
+console.log('Mining block 2....');
 sustainiaCoin.addBlock(new Block("2", "22/01/2018", {
     amount: 2
 }));
 
+console.log(JSON.stringify(sustainiaCoin, null, 2));
+console.log('Is blockchain valid? ' + sustainiaCoin.isChainValid());
+
+/* 
 // Valid
 console.log('Is blockchain valid? ' + sustainiaCoin.isChainValid());
 
@@ -120,3 +147,5 @@ console.log('Is blockchain valid? ' + sustainiaCoin.isChainValid());
 
 // Console.log out the result (stringify because the Blockchain is an object)
 // console.log(JSON.stringify(sustainiaCoin, null, 2));
+
+*/
