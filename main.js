9ed0352c1e974ca7ca57430f153aef7a2cc906c7
Pia Lyck Festersen <pialyck@gmail.com>
2018-01-23 14:08:45 +1100

    add calculateHash function to class Block

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Import SHA256 from crypto-js library
 const SHA256 = require('crypto-js/sha256');
 
 // Create class Block that defines that a block on our blockchain will look like.
@@ -7,19 +8,21 @@ class Block {
         timestamp = when was the block created.
         data = Object. Any sort of data that you want to associate with this block, eg. transaction details, if used for a cryptocurrency.
         previous hash = String. Contains the hash of the block before this one. Insures the integrity of the blockchain. Set to empty from default.
+        hash = String. Returned from calculateHash() function.
     */
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = ''; // will contain the hash of the block. This is calculated later on.
+        this.hash = this.calculateHash(); // contains the hash of the block. 
     }
 
     /*  Method for calculating the hash of the block, based on the block's properties.
         This will identify the block on the blockchain.    
     */
     calculateHash() {
-
+        // Returns a SHA256 hash (casted to a string)
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
