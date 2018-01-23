67155f92356cb114e38db6db6b3dc2ca531c12ef
Pia Lyck Festersen <pialyck@gmail.com>
2018-01-23 13:41:15 +1100

    add constructor to class Block

---
+++ main.js
@@ -0,0 +1,17 @@
+// Create class Block that defines that a block on our blockchain will look like.
+
+class Block {
+    /* The constructor receives the properties of the block, ie. index, timestamp, some data and previous hash.
+        index = optional. Tells us where the block is located on the chain.
+        timestamp = when was the block created.
+        data = Object. Any sort of data that you want to associate with this block, eg. transaction details, if used for a cryptocurrency.
+        previous hash = String. Contains the hash of the block before this one. Insures the integrity of the blockchain. Set to empty from default.
+    */
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = ''; // will contain the hash of the block. This is calculated later on.
+    }
+}
